ad7bf61ec10c2f79d14f96beada8c1e2aebb5ff4
duanlianqi <duanlianqi0528@gmail.com>
2026-01-07 16:53:55 +0800

    echo服务器和客户端

--- client.cpp
+++ client.cpp
@@ -2,9 +2,12 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <cstring>
+#include <unistd.h>
+#include "util.h"
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(sockfd == - 1, "socket create error!");
 
     struct sockaddr_in servAddr;
     bzero(&servAddr, sizeof(servAddr));
@@ -12,7 +15,31 @@ int main() {
     servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servAddr.sin_port = htons(12345);
 
-    connect(sockfd, (sockaddr *)&servAddr, sizeof(servAddr));
+    int ret = connect(sockfd, (sockaddr *)&servAddr, sizeof(servAddr));
+    errif(ret == -1, "socket connect error!");
 
+    while(true) {
+        char buf[1024] = {0};
+        std::cin >> buf;
+        ssize_t writeBytes = write(sockfd, buf, sizeof(buf));
+        if(writeBytes == -1) {
+            std::cout << "Server has already disconnected, can't write any more." << std::endl;
+            break;
+        }
+
+        bzero(buf, sizeof(buf));
+        ssize_t readBytes = read(sockfd, buf, sizeof(buf));
+        if(readBytes > 0) {
+            std::cout << "There has a message from server : " << sockfd << ", context is : " << buf << std::endl;
+        } else if(readBytes == 0) {
+            std::cout << "Server has already disconnected." << std::endl;
+            break;
+        } else if(readBytes == -1) {
+            errif(true, "socket read error!");
+            break;
+        }
+    }
+
+    close(sockfd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -2,9 +2,12 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <cstring>
+#include <unistd.h>
+#include "util.h"
 
 int main() {
     int servSockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(servSockfd == - 1, "socket create error!");
 
     struct sockaddr_in servAddr;
     bzero(&servAddr, sizeof(servAddr));
@@ -12,18 +15,39 @@ int main() {
     servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servAddr.sin_port = htons(12345);
 
-    bind(servSockfd, (sockaddr *)&servAddr, sizeof(servAddr));
+    int ret = bind(servSockfd, (sockaddr *)&servAddr, sizeof(servAddr));
+    errif(ret == -1, "socket bind error!");
 
-    listen(servSockfd, SOMAXCONN);
+    ret = listen(servSockfd, SOMAXCONN);
+    errif(ret == -1, "socket listen error!");
 
     struct sockaddr_in clientAddr;
     socklen_t clientAddrLen = sizeof(clientAddr);
     bzero(&clientAddr, clientAddrLen);
 
     int clientSockfd = accept(servSockfd, (sockaddr *)&clientAddr, &clientAddrLen);
+    errif(clientSockfd == -1, "socket accept error!");
 
-    std::cout << "new client fd : " << clientSockfd << ", IP : " << inet_ntoa(clientAddr.sin_addr)
+    std::cout << "New client fd : " << clientSockfd << ", IP : " << inet_ntoa(clientAddr.sin_addr)
               << ", Port : " << ntohs(clientAddr.sin_port) << ".\n";
 
+    while(true) {
+        char buf[1024] = {0};
+        ssize_t readBytes = read(clientSockfd, buf, sizeof(buf));
+        if(readBytes > 0) {
+            std::cout << "There has a message from client : " << clientSockfd << ", context is : " << buf << std::endl;
+            write(clientSockfd, buf, sizeof(buf));
+        } else if(readBytes == 0) {
+            std::cout << "Client disconnected, fd : " << clientSockfd << std::endl;
+            close(clientSockfd);
+            break;
+        } else if(readBytes == -1) {
+            close(clientSockfd);
+            errif(true, "socket read error!");
+            break;
+        }
+    }
+
+    close(servSockfd);
     return 0;
 }
